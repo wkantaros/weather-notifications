fcb5cc591e7fbef92bf53628093705a885b0b9cc
Will Kantaros <wkantaros@gmail.com>
2019-07-03 12:15:33 -0400

    small fixes to front end

--- weather_notification/send_notification.js
+++ weather_notification/send_notification.js
@@ -1,6 +1,9 @@
-const accountSid = 'AC1c4b6f56dc610ebd9a1bac37523f2d51';
-const authToken = '9826dfdd52ef25cc2ade67f6637529b7';
-const trialNumber = '+18452187873';
+require('dotenv').config();
+
+console.log(process.env.ACCOUNT_SID);
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+const trialNumber = process.env.TRIAL_NUMBER;
 
 const client = require('twilio')(accountSid, authToken);
 
@@ -9,7 +12,7 @@ const willNum = '+18455189455';
 
 client.messages
     .create({
-        body: `the free trial is $15 tho so ima just run the free trial till it dies i think`,
+        body: `test env`,
         from: trialNumber,
         to: willNum
     })
